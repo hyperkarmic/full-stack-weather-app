4be2dd3573ef5866cb0ac3e6dd6d1a50eec5f6b5
hyperkarma <randy_pan@yahoo.com>
2020-11-11 11:38:38 +0000

    models implemented and api files updated

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 //unusual format - means what?
+const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser')
 
@@ -14,5 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true}));
 //use API routes from the api folder - his means we put routes in here - and not the folder
 const apis = require("./api");
 app.use("/api", apis);
+
+//using process.env.DB to get the DB key and mongoose to connect to the database
+
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
