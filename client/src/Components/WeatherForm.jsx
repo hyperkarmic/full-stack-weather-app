e69a7a2c1d2a54fef270aa9a03043996033fe6a7
hyperkarma <randy_pan@yahoo.com>
2020-12-14 20:55:50 +0000

    changed json and added local storage capability to weather form

--- client/src/Components/WeatherForm.jsx
+++ client/src/Components/WeatherForm.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import {Form, Button, Row, Col, ButtonGroup, ToggleButton} from "react-bootstrap";
 
+import axios from 'axios';
+
 class WeatherForm extends Component {
     // default state values
     state = {
@@ -8,8 +10,47 @@ class WeatherForm extends Component {
         zipCodeInput: "98052"
     }
 
+    componentDidMount() {
+        this.refreshSavedWeather();
+    }
+
+    // Refreshes the current weather data for most recent zipcode - if it exists
+    
+    refreshSavedWeather = () => {
+        if (localStorage.getItem("zipCode")) {
+            axios.post("/api/weather", {
+            zipCode: localStorage.getItem("zipCode"),
+            tempMetric: localStorage.getItem("tempMetric")
+            }).then(d => {
+                localStorage.setIem("CurrentWeatherData", JSON.stringify(d.data00));
+            });
+        }
+    }
+    
     onChange = (e) => {
         this.setState({[e.target.name]: e.target.value});
+    }
+
+    saveFormData = (event) => {
+        event.preventDefault();
+
+        //Gets weather data from weather api and returns it to save into localstorage
+        axios.post("/api/weather", {
+            zipCode: this.state.zipCodeInput,
+            tempMetric: this.state.tempMetric
+        }).then(response => {
+            let weatherData = response.data;
+
+            this.saveToLocalStorage(weatherData);
+        });
+    }
+
+    //Save data from form to local storage
+
+    saveToLocalStorage = (weatherData) => {
+        localStorage.setItem("zipCode", this.state.zipCodeInput);
+        localStorage.setItem("tempMetric", this.state.tempMetric);
+        localStorage.setItem("CurrentWeatherData", JSON.stringify(weatherData));
     }
 
     render() {
